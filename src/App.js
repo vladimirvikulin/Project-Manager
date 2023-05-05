b5f48d7ccab47dd833169e8f5408de0ddbd6be1c
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-05-05 20:50:22 +0300

    added components to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/App.css'
 import Header from './components/Header';
+import List from './components/List';
 
 function App() {
+  const [tasks, setTasks] = useState (
+    [
+      {id: 1, title: 'Один', status: true},
+      {id: 2, title: 'Два', status: true},
+      {id: 3, title: 'Три', status: true},
+    ]
+  )
   return (
-    <div className= "App">
+    <div className= 'App'>
       <Header/>
+    <List title={'Список 1'} tasks={tasks}/>
     </div>
   );
 }
