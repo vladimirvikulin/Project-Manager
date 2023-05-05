82271ee596ee0d979e54a5cc223f46ea95e7c38d
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-05-05 19:33:09 +0300

    added header component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            My To-Do List
+        </div>
+    );
+};
+
+export default Header;
