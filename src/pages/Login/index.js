15a09a661ee8302b88e3c41d70e40410d587c668
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-05-20 17:30:31 +0300

    added Login component

---
+++ src/pages/Login/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
+import styles from "./Login.module.css";
+import MyButton from "../../components/ui/button/MyButton";
+
+export const Login = () => {
+  return (
+    <Paper classes={{ root: styles.root }}>
+      <Typography classes={{ root: styles.title }} variant="h5">
+        Вхід до облікового запису
+      </Typography>
+      <TextField
+        className={styles.field}
+        label="Email"
+        error
+        helperText="Невірно вказана пошта"
+        fullWidth
+      />
+      <TextField className={styles.field} label="Пароль" fullWidth />
+      <MyButton>
+        Увійти
+      </MyButton>
+    </Paper>
+  );
+};
