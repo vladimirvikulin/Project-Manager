3176aa9eb3eab4d8de2adeebd33104a03c571224
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-05-20 23:15:41 +0300

    added getting all groups from database

--- src/pages/ToDoList/List.js
+++ src/pages/ToDoList/List.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import ListGroup from '../../components/Group/Group';
 import AddGroupForm from '../../components/AddGroupForm'
 import styles from './List.module.css'
 import GroupsFilter from '../../components/GroupsFilter';
 import MyModal from '../../components/ui/modal/MyModal';
 import MyButton from '../../components/ui/button/MyButton';
-
+import { fetchGroups } from '../../redux/slices/groups';
 const List = (props) => {
+    const dispatch = useDispatch();
+    const { groups } = useSelector(state => state.groups);
+    const isGroupsLoading = groups.status === 'loading';
+    React.useEffect(() => {
+        dispatch(fetchGroups());
+    }, []);
     return (
         <div>
             <MyButton onClick={() => props.setModalGroupVisible(true)}>
@@ -16,19 +23,14 @@ const List = (props) => {
                 <AddGroupForm addGroup = {props.addGroup}/>
             </MyModal>
             <GroupsFilter filter={props.filter} setFilter={props.setFilter}/>
-            {props.sortedAndSearch.length
-                ?
                 <div>
-                     {props.sortedAndSearch.map((group) => 
+                     {isGroupsLoading? <h1 className={styles.list}>Завантаження</h1>:groups.items.map((group) => 
                      <ListGroup 
                         group={group} groups={props.groups} setGroups={props.setGroups} 
                         removeGroup={props.removeGroup} setLocalGroups={props.setLocalGroups}
                         key={group._id}/>
                     )}
                 </div>
-                : <h1 className={styles.list}>Список груп порожній</h1>
-            }
-            
         </div>
     );
 };
--- src/redux/slices/groups.js
+++ src/redux/slices/groups.js
@@ -1,4 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../axios.js';
+
+export const fetchGroups = createAsyncThunk('groups/fetchGroups', async () => {
+    const {data} = await axios.get('/groups');
+    return data;
+});
 
 const initialState = {
     groups: {
@@ -11,6 +17,20 @@ const groupsSlice = createSlice({
     name: 'groups',
     initialState,
     reducers: {},
+    extraReducers: {
+        [fetchGroups.pending]: (state) => {
+            state.groups.status = 'loading';
+            state.groups.items = [];
+        },
+        [fetchGroups.fulfilled]: (state, action) => {
+            state.groups.status = 'loaded';
+            state.groups.items = action.payload;
+        },
+        [fetchGroups.rejected]: (state) => {
+            state.groups.status = 'error';
+            state.groups.items = [];
+        },
+    }
 });
 
 export default groupsSlice.reducer
