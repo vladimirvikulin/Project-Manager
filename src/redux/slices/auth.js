27a145883ad74626293736bc79c4a715e1e235e3
vladimirvikulin <v.vikulin.im11@kpi.ua>
2023-05-21 12:37:56 +0300

    implemented data checking in the login window

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -1,4 +1,10 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from '../../axios.js';
+
+export const fetchAuth = createAsyncThunk('auth/fetchAuth', async (params) => {
+    const {data} = await axios.post('/auth/login', params);
+    return data;
+});
 
 const initialState = {
     data: null,
@@ -8,6 +14,22 @@ const initialState = {
 const authSlice = createSlice({
     name: 'auth',
     initialState,
+    extraReducers: {
+        [fetchAuth.pending]: (state) => {
+            state.status = 'loading';
+            state.data = null;
+        },
+        [fetchAuth.fulfilled]: (state, action) => {
+            state.status = 'loaded';
+            state.data = action.payload;
+        },
+        [fetchAuth.rejected]: (state) => {
+            state.status = 'error';
+            state.data = null;
+        },
+    }
 });
 
-export default authSlice.reducer
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
+export default authSlice.reducer;
